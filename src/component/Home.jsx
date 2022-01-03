bbd3c3e523a672daaa64778576d58df299cc39c1
Fahmiali99 <79827631+Fahmiali99@users.noreply.github.com>
2022-01-04 01:57:21 +0700

    done

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -59,84 +59,96 @@ const Home = () => {
         <div class="container w-100 d-flex justify-content-center mb-4 mt-4">
           <div class="row">
             <div class="col-sm">
-              <div className=" shadow-content">
-                <div className="container text-center p-3 color-content">
-                  <img src="/assets/img/home/fa1.png" alt="" width="50%" />
-                </div>
-                <div className="container text-center mt-3">
-                  <h4>Logo Design</h4>
-                  <button type="button" class="btn btn-success mt-3 mb-4">
-                    Order Now
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm">
-              <div className="shadow-content">
-                <div className="container text-center p-3 color-content">
-                  <img src="/assets/img/home/fa2.png" alt="" width="50%" />
-                </div>
-                <div className="container text-center mt-3">
-                  <h4>Flyer design</h4>
-                  <button type="button" class="btn btn-success mt-3 mb-4">
-                    Order Now
-                  </button>
+              <div className="mt-2 mb-2">
+                <div className=" shadow-content">
+                  <div className="container text-center p-3 color-content">
+                    <img src="/assets/img/home/fa1.png" alt="" width="50%" />
+                  </div>
+                  <div className="container text-center mt-3">
+                    <h4>Logo Design</h4>
+                    <button type="button" class="btn btn-success mt-3 mb-4">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
             <div class="col-sm">
-              <div className="shadow-content">
-                <div className="container text-center p-3 color-content">
-                  <img src="/assets/img/home/fa3.png" alt="" width="50%" />
+              <div className="mt-2 mb-2">
+                <div className="shadow-content">
+                  <div className="container text-center p-3 color-content">
+                    <img src="/assets/img/home/fa2.png" alt="" width="50%" />
+                  </div>
+                  <div className="container text-center mt-3">
+                    <h4>Flyer design</h4>
+                    <button type="button" class="btn btn-success mt-3 mb-4">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
-                <div className="container text-center mt-3">
-                  <h4>Poster design</h4>
-                  <button type="button" class="btn btn-success mt-3 mb-4">
-                    Order Now
-                  </button>
+              </div>
+            </div>
+            <div class="col-sm">
+              <div className="mt-2 mb-2">
+                <div className="shadow-content">
+                  <div className="container text-center p-3 color-content">
+                    <img src="/assets/img/home/fa3.png" alt="" width="50%" />
+                  </div>
+                  <div className="container text-center mt-3">
+                    <h4>Poster design</h4>
+                    <button type="button" class="btn btn-success mt-3 mb-4">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div class="container w-100 d-flex justify-content-center mb-4 ">
-          <div class="row">
+          <div class="row ">
             <div class="col-sm">
-              <div className="shadow-content">
-                <div className="container text-center p-3 color-content">
-                  <img src="/assets/img/home/fa4.png" alt="" width="50%" />
-                </div>
-                <div className="container text-center mt-3">
-                  <h4>Instagram feed design</h4>
-                  <button type="button" class="btn btn-success mt-3 mb-4">
-                    Order Now
-                  </button>
+              <div className="mb-2 mt-2">
+                <div className="shadow-content">
+                  <div className="container text-center p-3 color-content">
+                    <img src="/assets/img/home/fa4.png" alt="" width="50%" />
+                  </div>
+                  <div className="container text-center mt-3">
+                    <h4>Instagram feed design</h4>
+                    <button type="button" class="btn btn-success mt-3 mb-4">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
             <div class="col-sm">
-              <div className="shadow-content">
-                <div className="container text-center p-3 color-content">
-                  <img src="/assets/img/home/fa5.png" alt="" width="50%" />
-                </div>
-                <div className="container text-center mt-3">
-                  <h4>Video editor</h4>
-                  <button type="button" class="btn btn-success mt-3 mb-4">
-                    Order Now
-                  </button>
+              <div className="mt-2 mb-2">
+                <div className="shadow-content">
+                  <div className="container text-center p-3 color-content">
+                    <img src="/assets/img/home/fa5.png" alt="" width="50%" />
+                  </div>
+                  <div className="container text-center mt-3">
+                    <h4>Video editor</h4>
+                    <button type="button" class="btn btn-success mt-3 mb-4">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
             <div class="col-sm">
-              <div className="shadow-content">
-                <div className="container text-center p-3 color-content">
-                  <img src="/assets/img/home/fa6.png" alt="" width="50%" />
-                </div>
-                <div className="container text-center mt-3">
-                  <h4>Many more</h4>
-                  <button type="button" class="btn btn-success mt-3 mb-4">
-                    Order Now
-                  </button>
+              <div className="mt-2 mb-2">
+                <div className="shadow-content">
+                  <div className="container text-center p-3 color-content">
+                    <img src="/assets/img/home/fa6.png" alt="" width="50%" />
+                  </div>
+                  <div className="container text-center mt-3">
+                    <h4>Many more</h4>
+                    <button type="button" class="btn btn-success mt-3 mb-4">
+                      Order Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
