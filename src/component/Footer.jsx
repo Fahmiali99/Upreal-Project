995ff12472f7faf02c64bcf86ecf65c52fe943e8
Fahmiali99 <79827631+Fahmiali99@users.noreply.github.com>
2022-01-06 18:42:28 +0700

    footer hr footer do

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,95 +3,100 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer w-100 bg-warning ">
-      <div class="container p-4 pb-0">
-        <section class="">
-          <div class="row">
-            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4 font-weight-bold text-white text-decoration-none">
+      <div className="container p-4 pb-0">
+        <section className="">
+          <div className="row no-gutters-none">
+            <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold text-white text-decoration-none">
                 Company name
               </h6>
               <img
-                class="w-100 text-center"
+                className="w-100 text-center"
                 src="/assets/img/upreal.png"
                 alt=""
                 width="100%"
               />
             </div>
 
-            <hr class="w-100 clearfix d-md-none" />
+            <hr className="w-100  d-md-none" />
 
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4 font-weight-bold text-white">
+            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold text-white">
                 Products
               </h6>
               <p>
-                <a class="text-white text-decoration-none">MDBootstrap</a>
+                <a className="text-white text-decoration-none">MDBootstrap</a>
               </p>
               <p>
-                <a class="text-white text-decoration-none">MDWordPress</a>
+                <a className="text-white text-decoration-none">MDWordPress</a>
               </p>
               <p>
-                <a class="text-white text-decoration-none">BrandFlow</a>
+                <a className="text-white text-decoration-none">BrandFlow</a>
               </p>
               <p>
-                <a class="text-white text-decoration-none">Bootstrap Angular</a>
+                <a className="text-white text-decoration-none">
+                  Bootstrap Angular
+                </a>
               </p>
             </div>
 
-            <hr class="w-100 clearfix d-md-none" />
+            <hr className="w-100  d-md-none" />
 
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4 font-weight-bold text-white">
+            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold text-white">
                 Useful links
               </h6>
               <p>
-                <a class="text-white text-decoration-none">Your Account</a>
+                <a className="text-white text-decoration-none">Your Account</a>
               </p>
               <p>
-                <a class="text-white text-decoration-none">
+                <a className="text-white text-decoration-none">
                   Become an Affiliate
                 </a>
               </p>
               <p>
-                <a class="text-white text-decoration-none">Shipping Rates</a>
+                <a className="text-white text-decoration-none">
+                  Shipping Rates
+                </a>
               </p>
               <p>
-                <a class="text-white text-decoration-none">Help</a>
+                <a className="text-white text-decoration-none">Help</a>
               </p>
             </div>
 
-            <hr class="w-100 clearfix d-md-none" />
+            <hr className="w-100  d-md-none" />
 
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-              <h6 class="text-uppercase mb-4 font-weight-bold text-white">
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 className="text-uppercase mb-4 font-weight-bold text-white">
                 Contact
               </h6>
               <p>
-                <i class="fas fa-home mr-3 text-white"></i> New York, NY 10012,
-                US
+                <i className="fas fa-home mr-3 text-white"></i> New York, NY
+                10012, US
               </p>
               <p>
-                <i class="fas fa-envelope mr-3 text-white"></i> info@gmail.com
+                <i className="fas fa-envelope mr-3 text-white"></i>{" "}
+                info@gmail.com
               </p>
               <p>
-                <i class="fas fa-phone mr-3 text-white"></i> + 01 234 567 88
+                <i className="fas fa-phone mr-3 text-white"></i> + 01 234 567 88
               </p>
               <p>
-                <i class="fas fa-print mr-3 text-white"></i> + 01 234 567 89
+                <i className="fas fa-print mr-3 text-white"></i> + 01 234 567 89
               </p>
             </div>
           </div>
         </section>
 
-        <hr class="my-3" />
+        <hr className="my-3" />
 
-        <section class="p-3 pt-0">
-          <div class="row d-flex align-items-center">
-            <div class="col-md-7 col-lg-8 text-center text-md-start">
-              <div class="p-3 text-white">
+        <section className="p-3 pt-0">
+          <div className="row d-flex align-items-center">
+            <div className="col-md-7 col-lg-8 text-center text-md-start">
+              <div className="p-3 text-white">
                 © 2020 Copyright:
                 <a
-                  class="text-white text-decoration-none"
+                  className="text-white text-decoration-none"
                   href="https://mdbootstrap.com/"
                 >
                   MDBootstrap.com
@@ -99,37 +104,37 @@ const Footer = () => {
               </div>
             </div>
 
-            <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+            <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
               <a
-                class="btn btn-outline-light btn-floating m-1"
-                class="text-white"
+                className="btn btn-outline-light btn-floating m-1"
+                className="text-white"
                 role="button"
               >
-                <i class="fab fa-facebook-f"></i>
+                <i className="fab fa-facebook-f"></i>
               </a>
 
               <a
-                class="btn btn-outline-light btn-floating m-1"
-                class="text-white"
+                className="btn btn-outline-light btn-floating m-1"
+                className="text-white"
                 role="button"
               >
-                <i class="fab fa-twitter"></i>
+                <i className="fab fa-twitter"></i>
               </a>
 
               <a
-                class="btn btn-outline-light btn-floating m-1"
-                class="text-white"
+                className="btn btn-outline-light btn-floating m-1"
+                className="text-white"
                 role="button"
               >
-                <i class="fab fa-google"></i>
+                <i className="fab fa-google"></i>
               </a>
 
               <a
-                class="btn btn-outline-light btn-floating m-1"
-                class="text-white"
+                className="btn btn-outline-light btn-floating m-1"
+                className="text-white"
                 role="button"
               >
-                <i class="fab fa-instagram"></i>
+                <i className="fab fa-instagram"></i>
               </a>
             </div>
           </div>
