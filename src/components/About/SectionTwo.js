18151230d40dc25d150ea7b87582944d7d7ca28a
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-02-24 23:03:44 +0700

    on progress

--- src/components/About/SectionTwo.js
+++ src/components/About/SectionTwo.js
@@ -3,7 +3,7 @@ import React from "react";
 function SectionTwo() {
   return (
     <div className="aboutpage-2">
-      <div className=" container">
+      <div className=" container-fluid">
         <div className="row">
           <div className="col-sm  ">
             <div className="d-flex justify-content-center pt-3">
@@ -36,7 +36,7 @@ function SectionTwo() {
 
           <div className="col-sm ">
             <div className=" pt-3">
-              <div className="row row-cols-2 row-cols-md-2 g-2 pb-4">
+              <div className="row row-cols-2 row-cols-md-2 g-2 pb-4 bg-dark">
                 <div className="col  ">
                   <div className="p-2 bg-light  border-about ">
                     <div className="about-bol">
