34c735689e18ef7aa1bcc7b27266cd89fb55c5a6
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-02-28 22:36:09 +0700

    merge

---
+++ src/components/Blog/ButtonGroup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ButtonGroup() {
+  return (
+    <>
+      <button>One</button>
+      <button>Two</button>
+      <button>Three</button>
+    </>
+  );
+}
