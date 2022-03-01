7163e08415ff595d51ea4c4aa8b38049747f30e5
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-01 22:43:57 +0700

    merge

--- src/components/Blog/SectionBlogTwo.js
+++ src/components/Blog/SectionBlogTwo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ButtonGroup from "./ButtonGroup";
+
 
 function SectionBlogTwo() {
   return (
@@ -12,7 +12,7 @@ function SectionBlogTwo() {
         </p>
 
         <div className="row">
-          <ButtonGroup />
+
           <div className=" col-sm">
             <div className="container shadow-sm p-3 mb-2 bg-light rounded">
               <img src="/assets/img/blog/a.png" alt="" width="50%" />
