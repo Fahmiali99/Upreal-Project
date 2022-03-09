39fe1651bcf974a38cf098dc15938f0a9842ee14
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-10 01:15:47 +0700

    uniq

--- src/components/About/SectionFour.js
+++ src/components/About/SectionFour.js
@@ -14,6 +14,7 @@ function SectionFour() {
               <div class="col-sm">
                 <div className="responsive-map d-flex justify-content-center mt-3">
                   <iframe
+                    title="Location Owner"
                     className="rounded"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63188.879439919234!2d113.70701207306392!3d-8.171999945543913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695d6ab36ba7f%3A0xedcc47c2999d52f2!2sKec.%20Sumbersari%2C%20Kabupaten%20Jember%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1644839326072!5m2!1sid!2sid"
                     width="400"
