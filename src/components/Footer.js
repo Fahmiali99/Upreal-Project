e908def6cc165e98ef67117d298e1200e89aa834
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-10 00:24:57 +0700

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer w-100 bg-dark  ">
-      <div className="container-fluid w-100 d-flex justify-content-center">
+      <div className="container-fluid w-100 d-flex justify-content-center mt-5 mb-1">
         <div className=" col-10 pb-0">
           <section className="">
             <div className="row g-0">
               <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h6 className="text-uppercase mb-4 font-weight-bold text-white text-decoration-none">
-                  Company name
+                  <h4 className="text-white">Company name</h4>
                 </h6>
                 <img
                   className="w-100 text-center"
@@ -23,24 +23,26 @@ const Footer = () => {
 
               <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
                 <h6 className="text-uppercase mb-4 font-weight-bold text-white">
-                  Products
+                  <h4 className="text-white">Products</h4>
                 </h6>
                 <p>
-                  <a className="text-white text-decoration-none">Logo Design</a>
-                </p>
-                <p>
-                  <a className="text-white text-decoration-none">
-                    Flyer design
+                  <a className="text-white text-decoration-none sub-footer">
+                    <p className="sub-footer">Logo Design</p>
                   </a>
                 </p>
                 <p>
                   <a className="text-white text-decoration-none">
-                    Poster design
+                    <p className="sub-footer">Flyer design</p>
                   </a>
                 </p>
                 <p>
                   <a className="text-white text-decoration-none">
-                    Instagram feed design
+                    <p className="sub-footer">Poster design</p>
+                  </a>
+                </p>
+                <p>
+                  <a className="text-white text-decoration-none">
+                    <p className="sub-footer">Instagram feed design</p>
                   </a>
                 </p>
               </div>
@@ -49,11 +51,13 @@ const Footer = () => {
 
               <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
                 <h6 className="text-uppercase mb-4 font-weight-bold text-white">
-                  About
+                  <h4 className="text-white">About</h4>
                 </h6>
                 <p>
                   <a className="text-white text-decoration-none">
-                    Anda dapat menemukan banyak jasa di sini
+                    <p className="sub-footer">
+                      Anda dapat menemukan banyak jasa di sini
+                    </p>
                   </a>
                 </p>
               </div>
@@ -62,34 +66,40 @@ const Footer = () => {
 
               <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h6 className="text-uppercase mb-4 font-weight-bold text-white">
-                  Contact
+                  <h4 className="text-white">Contact</h4>
                 </h6>
                 <p className="text-white">
-                  <i className="fas fa-home mr-3 text-white"></i> Sumbersari,
-                  Jember
+                  <p className="sub-footer">
+                    <i className="fas fa-home mr-3 text-white"></i> Sumbersari,
+                    Jember
+                  </p>
                 </p>
                 <p className="text-white">
-                  <i className="fas fa-envelope mr-3 text-white"></i>
-                  mekkipatria@gmail.com
+                  <p className="sub-footer">
+                    <i className="fas fa-envelope mr-3 text-white"></i>
+                    mekkipatria@gmail.com
+                  </p>
                 </p>
                 <p className="text-white">
-                  <i className="fas fa-phone mr-3 text-white"></i>{" "}
-                  +6281252188672
+                  <p className="sub-footer">
+                    <i className="fas fa-phone mr-3 text-white"></i>{" "}
+                    +6281252188672
+                  </p>
                 </p>
                 <div class=" mb-4 mb-md-0">
-                  <h3>Subscribe</h3>
-                  <p class="mb-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Eaque dolor ducimus doloremque earum ullam. Architecto
-                    voluptatum odio magni.
-                  </p>
+                  <h3 className="text-white">Subscribe</h3>
+
                   <form action="#" class="subscribe">
                     <input
                       type="text"
                       class="form-control"
                       placeholder="Enter your e-mail"
                     />
-                    <input type="submit" class="btn btn-submit" value="Send" />
+                    <input
+                      type="submit"
+                      class="btn btn-submits text-white"
+                      value="Send"
+                    />
                   </form>
                 </div>
               </div>
@@ -112,37 +122,33 @@ const Footer = () => {
                 </div>
               </div>
 
-              <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+              <div className="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end ">
                 <a
-                  className="btn btn-outline-light btn-floating m-1"
-                  className="text-white"
+                  className="btn btn-floating m-1 border-0 text-white"
                   role="button"
                   href="https://www.instagram.com/uprealhomecreative/"
                 >
-                  <i className="fab fa-instagram mr-2"></i>
+                  <i className="fab fa-instagram mr-2 icon-media-footer"></i>
                 </a>
                 <a
-                  className="btn btn-outline-light btn-floating m-1"
-                  className="text-white"
+                  className="btn btn-floating m-1 border-0 text-white"
                   role="button"
                 >
-                  <i className="fab fa-facebook-f mr-2"></i>
-                </a>
-
-                <a
-                  className="btn btn-outline-light btn-floating m-1"
-                  className="text-white"
-                  role="button"
-                >
-                  <i className="fab fa-twitter mr-2"></i>
+                  <i className="fab fa-facebook-f mr-2 icon-media-footer"></i>
                 </a>
 
                 <a
-                  className="btn btn-outline-light btn-floating m-1"
-                  className="text-white"
+                  className="btn btn-floating m-1 border-0 text-white"
                   role="button"
                 >
-                  <i className="fab fa-google mr-2"></i>
+                  <i className="fab fa-twitter mr-2 icon-media-footer"></i>
+                </a>
+
+                <a
+                  className="btn btn-floating m-1 border-0 text-white"
+                  role="button"
+                >
+                  <i className="fab fa-google mr-2 icon-media-footer"></i>
                 </a>
               </div>
             </div>
