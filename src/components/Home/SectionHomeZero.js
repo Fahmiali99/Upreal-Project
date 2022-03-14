ab3616ff16614391abbf5ce2093c34b82e5a49bc
Fahmi Ali Husni <79827631+Fahmiali99@users.noreply.github.com>
2022-03-14 21:08:39 +0700

    tes

--- src/components/Home/SectionHomeZero.js
+++ src/components/Home/SectionHomeZero.js
@@ -3,41 +3,43 @@ import React from "react";
 const SectionHomeZero = () => {
   return (
     <div classNameName="homepage ">
-      <div className="w-100 d-flex justify-content-center ">
-        <div className="col-md-10 ">
-          <div className=" row flex-center ">
-            <div class="col-sm order-md-1 ">
-              <div className=" w-100">
-                <div className="d-flex justify-content-center mt-4 mb-3">
-                  <img
-                    class="img-fluid "
-                    src="/assets/img/home/baner.svg"
-                    alt=""
-                    width="95%"
-                  />
+      <div className="homessss">
+        <div className="w-100 d-flex justify-content-center ">
+          <div className="col-md-10 ">
+            <div className=" row flex-center ">
+              <div class="col-sm order-md-1 ">
+                <div className=" w-100">
+                  <div className="d-flex justify-content-center mt-4 mb-3">
+                    <img
+                      class="img-fluid "
+                      src="/assets/img/home/baner.svg"
+                      alt=""
+                      width="95%"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div class="col-sm  text-center text-md-start d-flex align-items-center">
-              <div className="">
-                <div className="title-inside">
-                  <h1 class="">
-                    Butuh desain ?<br />
-                    <span class="text-secondary ">Upreal</span> adalah
-                    solusinya.
-                  </h1>
-                  <p class="mt-3 mb-4">
-                    <span class="fw-medium">Upreal </span> menyediakan banyak
-                    jasa yang dapat di pilih sesuai yang di butuhkan konsumen.
-                    Jika anda ingin meneruskan bisa klik I'm Order.{" "}
-                  </p>
-                  <a
-                    class="btn btn-lg btn-danger hover-top btn-glow"
-                    href="#layanan"
-                  >
-                    I'm Order{" "}
-                  </a>
+              <div class="col-sm  text-center text-md-start d-flex align-items-center">
+                <div className="">
+                  <div className="title-inside">
+                    <h1 class="">
+                      Butuh desain ?<br />
+                      <span class="text-secondary ">Upreal</span> adalah
+                      solusinya.
+                    </h1>
+                    <p class="mt-3 mb-4">
+                      <span class="fw-medium">Upreal </span> menyediakan banyak
+                      jasa yang dapat di pilih sesuai yang di butuhkan konsumen.
+                      Jika anda ingin meneruskan bisa klik I'm Order.{" "}
+                    </p>
+                    <a
+                      class="btn btn-lg btn-danger hover-top btn-glow"
+                      href="#layanan"
+                    >
+                      I'm Order{" "}
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
